90d27e2b43d3ab111129d7a9b32fc132008eabaf
Aidens-fox <rootwang838@gmail.com>
2026-04-21 22:07:35 +0800

    xinitrc set slock

--- appinstall.sh
+++ appinstall.sh
@@ -15,7 +15,7 @@ sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xorg-xset xsel x
 echo "install Font"
 sudo pacman -S ttf-sarasa-gothic  noto-fonts  noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono-nerd
 echo "install yazi emacs nvim"
-sudo pacman -S  neovim mpv yazi ueberzugpp emacs ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick unzip nwg-look lxsession imv
+sudo pacman -S  neovim mpv yazi ueberzugpp emacs ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick unzip nwg-look lxsession imv slock udisks2 trash-cli 
 echo "install zsh"
 sudo pacman -S zsh-completions zsh
 chsh -s /usr/bin/zsh
