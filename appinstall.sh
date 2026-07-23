ad079c40ef971caccdde426190610715731ad0d1
Aidens-fox <rootwang838@gmail.com>
2026-07-23 22:54:43 +0800

    set dunst

--- appinstall.sh
+++ appinstall.sh
@@ -15,7 +15,7 @@ sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xorg-xset xsel x
 echo "install Font"
 sudo pacman -S ttf-sarasa-gothic  noto-fonts  noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono-nerd
 echo "install app"
-sudo pacman -S  neovim mpv yazi ueberzugpp ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick unzip nwg-look lxsession imv slock udisks2 trash-cli brightnessctl caja flameshot  
+sudo pacman -S  neovim mpv yazi ueberzugpp ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick unzip nwg-look lxsession imv slock udisks2 trash-cli brightnessctl caja flameshot batsignal dunst nm-connection-editor network-manager-applet 
 echo "install zsh"
 sudo pacman -S zsh-completions zsh
 chsh -s /usr/bin/zsh
@@ -41,6 +41,8 @@ cd $HOME/dwm
 cp -r nvim $HOME/.config/
 echo "cp flameshot"
 cp -r flameshot $HOME/.config/
+echo "cp dunst"
+cp -r dunst $HOME/.config/
 echo "cp wallpaper"
 cp -r 壁纸 $HOME/
 echo "cp yazi"
